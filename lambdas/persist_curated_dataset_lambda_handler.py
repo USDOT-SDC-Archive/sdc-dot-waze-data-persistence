9a99c472102234075c38cc0231a5de9d3abdd606
HannahKent <hkent@acrotron.com>
2019-07-17 08:26:17 -0600

    SDC-529 Coverage increased to 90%, added requirements.txt

--- lambdas/persist_curated_dataset_lambda_handler.py
+++ lambdas/persist_curated_dataset_lambda_handler.py
@@ -6,19 +6,28 @@ import boto3
 from base64 import *
 import uuid
 
-ENCRYPTED_REDSHIFT_MASTER_PASSWORD = os.environ['REDSHIFT_MASTER_PASSWORD']
-DECRYPTED_REDSHIFT_MASTER_PASSWORD = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_REDSHIFT_MASTER_PASSWORD))['Plaintext'].decode('utf-8')
 FUNCTION_LOGIC = os.environ['FUNCTION_LOGIC']
 REDSHIFT_SQL_DIR = os.path.join('/tmp/', 'redshift_sql')
 CONFIG_BUCKET = os.environ['CONFIG_BUCKET']
 SQL_KEY_PREFIX = os.environ['SQL_KEY_PREFIX']
-s3Resource = boto3.resource('s3')
 
 if not os.path.exists(REDSHIFT_SQL_DIR):
     os.mkdir(REDSHIFT_SQL_DIR)
 
+
 def __persist_records_to_redshift(manifest_s3key_name, table_name, sql_file_name, batch_id, is_historical):
+    """
+
+    :param manifest_s3key_name:
+    :param table_name:
+    :param sql_file_name:
+    :param batch_id:
+    :param is_historical:
+    :return:
+    """
     try:
+
+        s3Resource = boto3.resource('s3')  # region_name='us-east-1'
         LoggerUtility.logInfo("Started persistence for table_name - {}".format(table_name))
         curated_bucket = os.environ['CURATED_BUCKET_NAME']
         LoggerUtility.logInfo("Manifest s3 key = {}".format(manifest_s3key_name))
@@ -26,21 +35,21 @@ def __persist_records_to_redshift(manifest_s3key_name, table_name, sql_file_name
         # Download the file from S3 to REDSHIFT_SQL_DIR path
         query_file_temp_name = str(uuid.uuid4()) + sql_file_name
         s3Resource.Bucket(CONFIG_BUCKET).download_file(SQL_KEY_PREFIX + "/" + sql_file_name,
-                                                    REDSHIFT_SQL_DIR + "/" + query_file_temp_name)
+                                                       REDSHIFT_SQL_DIR + "/" + query_file_temp_name)
         LoggerUtility.logInfo("Downloaded file from S3 - {}".format(query_file_temp_name))
         dw_schema_name = "dw_waze"
         elt_schema_name = "elt_waze"
 
-        if(is_historical):
+        if is_historical:
             dw_schema_name = "dw_waze_history"
             elt_schema_name = "elt_waze_history"
 
         redshift_manager.execute_from_file(query_file_temp_name,
-                                                   curated_bucket_name=curated_bucket,
-                                                   manifest_curated_key=manifest_s3key_name,
-                                                   batchIdValue=batch_id,
-                                                   dw_schema_name=dw_schema_name,
-                                                   elt_schema_name=elt_schema_name)
+                                           curated_bucket_name=curated_bucket,
+                                           manifest_curated_key=manifest_s3key_name,
+                                           batchIdValue=batch_id,
+                                           dw_schema_name=dw_schema_name,
+                                           elt_schema_name=elt_schema_name)
         # delete the file once executed
         os.remove(REDSHIFT_SQL_DIR + "/" + query_file_temp_name)
     except Exception as e:
@@ -49,11 +58,15 @@ def __persist_records_to_redshift(manifest_s3key_name, table_name, sql_file_name
         raise
 
 
-
 def __make_redshift_manager():
+
+    encrypted_redshift_master_password = os.environ['REDSHIFT_MASTER_PASSWORD']
+    decrypted_redshift_master_password = boto3.client('kms').decrypt(
+        CiphertextBlob=b64decode(encrypted_redshift_master_password))['Plaintext'].decode('utf-8')
+
     redshift_connection = RedshiftConnection(
         os.environ['REDSHIFT_MASTER_USERNAME'],
-        DECRYPTED_REDSHIFT_MASTER_PASSWORD,
+        decrypted_redshift_master_password,
         os.environ['REDSHIFT_JDBC_URL']
     )
     query_loader = TemplateLoader(REDSHIFT_SQL_DIR)
@@ -67,10 +80,10 @@ def __make_redshift_manager():
 
 def persist_curated_datasets(event, context, batch_id):
     LoggerUtility.setLevel()
-    
-    tableName=event["tablename"]
-    maifestUrlParameter=tableName+"url"
-    manifestURl=event[maifestUrlParameter]
+
+    tableName = event["tablename"]
+    manifestUrlParameter = tableName + "url"
+    manifestURL = event[manifestUrlParameter]
     is_historical = event["is_historical"] == 'true'
-    sql_file_name=FUNCTION_LOGIC+"_"+tableName+".sql"
-    __persist_records_to_redshift(manifestURl,tableName,sql_file_name, batch_id, is_historical)
+    sql_file_name = FUNCTION_LOGIC + "_" + tableName + ".sql"
+    __persist_records_to_redshift(manifestURL, tableName, sql_file_name, batch_id, is_historical)
---
+++ tests/test_persist_curated_dataset.py
@@ -0,0 +1,139 @@
+import os
+
+os.environ['FUNCTION_LOGIC'] = 'FUNCTION_LOGIC'
+os.environ['CONFIG_BUCKET'] = 'CONFIG_BUCKET'
+os.environ['SQL_KEY_PREFIX'] = 'SQL_KEY_PREFIX'
+os.environ['CURATED_BUCKET_NAME'] = "curated_bucket_name"
+
+import pytest
+import boto3
+from lambdas import persist_curated_dataset_lambda_handler
+from unittest import mock
+
+manifest_s3key_name = "manifest_s3key_name"
+table_name = "table_name"
+sql_file_name = "sql_file_name"
+batch_id = "batch_id"
+
+
+def Any(cls):
+    class Any(cls):
+        def __eq__(self, other):
+            return True
+    return Any()
+
+
+def test__make_redshift_manager(monkeypatch):
+    os.environ['REDSHIFT_MASTER_PASSWORD'] = "super secret"
+    os.environ['REDSHIFT_MASTER_USERNAME'] = "tester"
+    os.environ['REDSHIFT_JDBC_URL'] = "www.url.com"
+    os.environ['REDSHIFT_ROLE_ARN'] = "redshift_role_arn"
+
+    class MockBoto3Client:
+        def __init__(*args, **kwargs):
+            pass
+
+        @staticmethod
+        def decrypt(*args, **kwargs):
+            return {'Plaintext': os.environ['REDSHIFT_MASTER_PASSWORD'].encode('utf-8')}
+
+    def mock_b64decode(*args, **kwargs):
+        pass
+
+    class MockRedshiftConnection:
+
+        def __init__(self, user, password, redshift_jdbc_url):
+
+            self.user = user
+            self.password = password
+            self.redshift_jdbc_url = redshift_jdbc_url
+
+    class MockRedshiftManager:
+
+        def __init__(self, region_name, redshift_role_arn, redshift_connection, query_loader):
+
+            self.region_name = region_name
+            self.redshift_role_arn = redshift_role_arn
+            self.redshift_connection = redshift_connection
+            self.query_loader = query_loader
+
+    # try overloading TemplateLoader init with monkeypatch and see what happens
+    def mock_template_loader(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(boto3, "client", MockBoto3Client)
+
+    persist_curated_dataset_lambda_handler.b64decode = mock_b64decode
+    persist_curated_dataset_lambda_handler.RedshiftConnection = MockRedshiftConnection
+    persist_curated_dataset_lambda_handler.RedshiftManager = MockRedshiftManager
+    persist_curated_dataset_lambda_handler.TemplateLoader = mock_template_loader
+
+    redshift_manager = persist_curated_dataset_lambda_handler.__make_redshift_manager()
+
+    assert redshift_manager.region_name == 'us-east-1'
+    assert redshift_manager.redshift_role_arn == os.environ['REDSHIFT_ROLE_ARN']
+    assert redshift_manager.redshift_connection.user == os.environ['REDSHIFT_MASTER_USERNAME']
+    assert redshift_manager.redshift_connection.password == os.environ['REDSHIFT_MASTER_PASSWORD']
+    assert redshift_manager.redshift_connection.redshift_jdbc_url == os.environ['REDSHIFT_JDBC_URL']
+    assert redshift_manager.query_loader == mock_template_loader()
+
+
+def test__persist_records_to_redshift_historical():
+
+    is_historical = True
+
+    boto3.resource = mock.MagicMock()
+    os.remove = mock.MagicMock()
+
+    persist_curated_dataset_lambda_handler.__make_redshift_manager = mock.MagicMock()
+
+    persist_curated_dataset_lambda_handler.__persist_records_to_redshift(
+        manifest_s3key_name, table_name, sql_file_name, batch_id, is_historical)
+
+    persist_curated_dataset_lambda_handler.__make_redshift_manager().execute_from_file.assert_called_once_with(
+        Any(str),
+        batchIdValue='batch_id',
+        curated_bucket_name='curated_bucket_name',
+        dw_schema_name='dw_waze_history',
+        elt_schema_name='elt_waze_history',
+        manifest_curated_key='manifest_s3key_name'
+    )
+
+
+def test__persist_records_to_redshift_not_historical():
+
+    is_historical = False
+
+    boto3.resource = mock.MagicMock()
+    os.remove = mock.MagicMock()
+
+    persist_curated_dataset_lambda_handler.__make_redshift_manager = mock.MagicMock()
+
+    persist_curated_dataset_lambda_handler.__persist_records_to_redshift(
+        manifest_s3key_name, table_name, sql_file_name, batch_id, is_historical)
+
+    persist_curated_dataset_lambda_handler.__make_redshift_manager().execute_from_file.assert_called_once_with(
+        Any(str),
+        batchIdValue='batch_id',
+        curated_bucket_name='curated_bucket_name',
+        dw_schema_name='dw_waze',
+        elt_schema_name='elt_waze',
+        manifest_curated_key='manifest_s3key_name'
+    )
+
+
+def test_persist_curated_datasets():
+
+    event = {
+        "tablename": "tablename",
+        "is_historical": "true",
+        "tablenameurl": "tablenameurl"
+    }
+
+    persist_curated_dataset_lambda_handler.__persist_records_to_redshift = mock.MagicMock()
+
+    persist_curated_dataset_lambda_handler.persist_curated_datasets(event, None, batch_id)
+
+    persist_curated_dataset_lambda_handler.__persist_records_to_redshift.assert_called_with(
+        "tablenameurl", "tablename", "FUNCTION_LOGIC_tablename.sql", batch_id, True
+    )
