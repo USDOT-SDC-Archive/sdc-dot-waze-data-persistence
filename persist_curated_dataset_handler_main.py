5f80ce4adc1ae9fe6b53eee5188724c9acb6218d
HannahKent <hkent@acrotron.com>
2019-10-16 15:35:20 -0600

    SDC-921 removed code smells

--- persist_curated_dataset_handler_main.py
+++ persist_curated_dataset_handler_main.py
@@ -1,9 +1,9 @@
 from lambdas.persist_curated_dataset_lambda_handler import *
 
 
-def lambda_handler(event, context):
-    LoggerUtility.setLevel()
-    LoggerUtility.logInfo("Batch Id from previous lambda - {}".format(event['batchId']))
+def lambda_handler(event, *args, **kwargs):
+    LoggerUtility.set_level()
+    LoggerUtility.log_info("Batch Id from previous lambda - {}".format(event['batchId']))
     batch_id = event['batchId']
-    persist_curated_datasets(event, context, batch_id)
+    persist_curated_datasets(event, batch_id)
     return event
