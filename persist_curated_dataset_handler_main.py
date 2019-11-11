3e8adf52b24e277385ede780b75077e09e93f516
HannahKent <hkent@acrotron.com>
2019-11-11 13:59:26 -0700

    Added context param back to lambda_handler - cleaned up test code as well

--- persist_curated_dataset_handler_main.py
+++ persist_curated_dataset_handler_main.py
@@ -1,7 +1,7 @@
 from lambdas.persist_curated_dataset_lambda_handler import *
 
 
-def lambda_handler(event, *args, **kwargs):
+def lambda_handler(event, context):
     LoggerUtility.set_level()
     LoggerUtility.log_info("Batch Id from previous lambda - {}".format(event['batchId']))
     batch_id = event['batchId']
